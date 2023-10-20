fe989ab80da49b47d97455c448ee0ac0eefc440c
Anaëlle Laurans <16943368+Laurans@users.noreply.github.com>
2023-10-20 09:05:39 +0200

    🎨 Format code with black and isort

--- src/main/scala/QualityMetrics/ConcurrentQueryExecutor.scala
+++ src/main/scala/QualityMetrics/ConcurrentQueryExecutor.scala
@@ -9,29 +9,26 @@ import java.sql.DriverManager
 def sleep(millis: Long) = Thread.sleep(millis)
 
 object ConcurrentQueryExecutor:
-    def executeQueries(queries: List[String]): Unit = {
-        val futures: List[Future[Try[String]]] = queries.map(str => 
-            Future {
-                Try {
-                    val duration = (math.random * 1000).toLong
-                    sleep(duration)
-                    val result = s"Result of $str"
-                    if str  == "table3" then throw RuntimeException(s"$str -> $duration")
-                    result
-                }
-            }
-        )
-        val aggregatedFuture: Future[List[Try[String]]] = Future.sequence(futures)
-        aggregatedFuture.onComplete {
-        case Success(results) =>
-            println("All queries completed successfully!")
-            results.foreach(result => println(s"Result: $result"))
-        case Failure(throwable) =>
-            println(s"FAILURE! $throwable")
+  def executeQueries(queries: List[String]): Unit = {
+    val futures: List[Future[Try[String]]] = queries.map(str =>
+      Future {
+        Try {
+          val duration = (math.random * 1000).toLong
+          sleep(duration)
+          val result = s"Result of $str"
+          if str == "table3" then throw RuntimeException(s"$str -> $duration")
+          result
         }
-
+      })
+    val aggregatedFuture: Future[List[Try[String]]] = Future.sequence(futures)
+    aggregatedFuture.onComplete {
+      case Success(results) =>
+        println("All queries completed successfully!")
+        results.foreach(result => println(s"Result: $result"))
+      case Failure(throwable) =>
+        println(s"FAILURE! $throwable")
     }
 
-    def executeQuery(query: String): Unit = {
-        
-    }
+  }
+
+  def executeQuery(query: String): Unit = {}
