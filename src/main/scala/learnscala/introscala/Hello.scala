0347a8492a7221c5336b871c1b3b75ebf5960d7b
Anaëlle Laurans <16943368+Laurans@users.noreply.github.com>
2023-11-05 18:11:05 +0100

    Delete src/main/scala/learnscala/introscala directory

--- src/main/scala/learnscala/introscala/Hello.scala
+++
@@ -1,5 +0,0 @@
-package learnscala.introscala
-
-@main def Hello(params: String*): Unit =
-  val output = params.map(_.toUpperCase).mkString(" ")
-  println(output)
--- src/main/scala/learnscala/introscala/shapes/Messages.scala
+++
@@ -1,7 +0,0 @@
-package learnscala.introscala.shapes
-
-
-sealed trait Message
-case class Draw(shape: Shape) extends Message
-case class Response(message: String) extends Message
-case class Exit() extends Message
